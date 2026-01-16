37795a9c061477c7d82b6549658ada7fc2a0b821
Khaleesor Ajetunmobi <ajetunmobikhaleesor008@gmail.com>
2026-01-16 07:02:44 +0100

    rearranged files

--- Landing Page.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
-});
